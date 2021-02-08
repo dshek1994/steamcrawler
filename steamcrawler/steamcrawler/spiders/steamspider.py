6f0043cf05841871491a217f5acc35a278686866
dshek1994 <danielshekari94@gmail.com>
2021-02-08 02:24:08 -0600

    Created XPATH queries to sift through steam alias, avatar, friends, steam ids ...

--- steamcrawler/steamcrawler/spiders/steamspider.py
+++ steamcrawler/steamcrawler/spiders/steamspider.py
@@ -7,5 +7,26 @@ class SteamSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        with open('profile.html', 'wb') as html_file:
-            html_file.write(response.body)
+        yield {
+            'alias': response.xpath("//span[@class='actual_persona_name']/text()").get(),
+            'avatar': response.xpath("//div[@class='playerAvatarAutoSizeInner']//img/@src").get(),
+            'friend_link': response.xpath("//div[@class='profile_friend_links profile_count_link_preview_ctn responsive_groupfriends_element']//@href").get(),
+        }
+
+        url = response.xpath("//div[@class='profile_friend_links profile_count_link_preview_ctn responsive_groupfriends_element']//@href").get()
+        yield scrapy.Request(url, callback = self.parseFriends)
+    
+    def parseFriends(self, response):
+        friend_urls = response.xpath("//div[@class='friends_container']//@href").getall()
+        
+        for url in friend_urls:
+            yield scrapy.Request(url, callback = self.parse)
+        # yield {
+        #     'friends': response.xpath("//div[@class='friends_container']//@href").getall()
+        # }
+
+
+
+    # def parse(self, response):
+    #     with open('profile.html', 'wb') as html_file:
+    #         html_file.write(response.body)
